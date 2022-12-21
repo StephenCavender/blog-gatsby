5bbfe2da6a3ee6e9edd379c70f7f26efcb19c8c6
Stephen Cavender <6619472+StephenCavender@users.noreply.github.com>
2022-12-21 15:54:39 -0500

    feat: add card component, add recent posts to index

---
+++ src/components/card.js
@@ -0,0 +1,28 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+
+const Card = ({ link, title, thumbnail, date, dateModified, excerpt }) => {
+  return (
+    <>
+      <div className="rounded border border-slate-500 bg-slate-200 shadow-lg transition-all duration-500 ease-in-out hover:-translate-y-2 hover:shadow-slate-400">
+        <Link to={link} className="block p-4">
+          <div className="flex flex-col items-center gap-4 sm:flex-row">
+            <div className="h-[120px] w-[180px] shrink-0 overflow-hidden rounded shadow-lg">
+              <GatsbyImage alt={title} image={getImage(thumbnail)} />
+            </div>
+            <div>
+              <time className="font-semibold text-slate-600">
+                {dateModified ? dateModified : date}
+              </time>
+              <h3 className="mt-3 mb-1 text-xl">{title}</h3>
+              <p className="text-base text-slate-300">{excerpt}</p>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </>
+  )
+}
+
+export default Card
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,104 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
+import { Link, graphql } from 'gatsby'
 import { SEO } from '../components/seo'
 import Bio from '../images/bio.jpg'
 import { useSiteMetadata } from '../hooks/use-site-metadata'
 import GitHub from '../svg/github.svg'
 import Dev from '../svg/devdotto.svg'
 import LinkedIn from '../svg/linkedin.svg'
+import Card from '../components/card'
 
 const Index = ({ data }) => {
   const { githubUrl, devUrl, linkedInUrl } = useSiteMetadata()
 
   return (
-    <div className="mb-8">
-      <img alt="bio" src={Bio} className="float-right w-[180px] rounded-full" />
-      <h2 className="mb-6 text-4xl">Hi</h2>
-      <p className="my-2">My name is Steve.</p>
-      <p className="my-2">
-        I'm a software engineer at <Link to="https://tele.vet">Televet</Link>.
-      </p>
-      <p className="my-2">
-        I enjoy coding, guitar, crossfit, video games, whiskey, and{' '}
-        <Link to="https://strenuouslife.co">doing hard things</Link>.
-      </p>
-      <div className="mt-12 flex justify-around">
-        <a href={githubUrl}>
-          <GitHub alt="github link" className="w-12 fill-current " />
-        </a>
-        <a href={devUrl}>
-          <Dev alt="dev to link" className="w-12 fill-current" />
-        </a>
-        <a href={linkedInUrl}>
-          <LinkedIn alt="linked in link" className="w-12 fill-current" />
-        </a>
+    <>
+      <div className="mb-8">
+        <img
+          alt="bio"
+          src={Bio}
+          className="float-right w-[180px] rounded-full"
+        />
+        <h2 className="mb-6 text-4xl">Hi</h2>
+        <p className="my-2">My name is Steve.</p>
+        <p className="my-2">
+          I'm a software engineer at <Link to="https://tele.vet">Televet</Link>.
+        </p>
+        <p className="my-2">
+          I enjoy coding, guitar, crossfit, video games, whiskey, and{' '}
+          <Link to="https://strenuouslife.co">doing hard things</Link>.
+        </p>
+        <div className="mt-12 flex justify-around">
+          <a href={githubUrl}>
+            <GitHub alt="github link" className="w-12 fill-current " />
+          </a>
+          <a href={devUrl}>
+            <Dev alt="dev to link" className="w-12 fill-current" />
+          </a>
+          <a href={linkedInUrl}>
+            <LinkedIn alt="linked in link" className="w-12 fill-current" />
+          </a>
+        </div>
       </div>
-    </div>
+      <div>
+        <h3 className="mb-4 text-xl font-bold">Recent Posts</h3>
+        <ul
+          className="mb-4"
+          style={{
+            listStyleType: 'none',
+            padding: 0,
+            display: 'grid',
+            gap: '2rem',
+          }}
+        >
+          {data.allMdx.nodes.map(
+            ({ id, excerpt, frontmatter, fields: { slug } }) => (
+              <li key={id}>
+                <Card
+                  link={slug}
+                  title={frontmatter.title}
+                  thumbnail={frontmatter.featuredImage?.path?.childImageSharp}
+                  date={frontmatter.date}
+                  dateModified={frontmatter.dateModified}
+                  excerpt={excerpt}
+                />
+              </li>
+            )
+          )}
+        </ul>
+
+        <div className="my-6 text-center">
+          <Link to="/posts">More posts...</Link>
+        </div>
+      </div>
+    </>
   )
 }
+
+export const query = graphql`
+  query {
+    allMdx {
+      nodes {
+        fields {
+          slug
+        }
+        excerpt(pruneLength: 100)
+        frontmatter {
+          title
+          date(formatString: "MMMM Do, YYYY")
+          featuredImage {
+            path {
+              childImageSharp {
+                gatsbyImageData(width: 800)
+              }
+            }
+            caption
+          }
+        }
+      }
+    }
+  }
+`
 
 export default Index
 
