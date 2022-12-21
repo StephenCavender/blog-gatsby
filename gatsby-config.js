1e626f7906de410c7a5a1f8d817215cfdfe63263
Stephen Cavender <6619472+StephenCavender@users.noreply.github.com>
2022-12-21 13:27:32 -0500

    feat: support featured images sourced from frontmatter

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,12 +7,24 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`
   },
   plugins: [
+    'gatsby-plugin-image',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     'gatsby-plugin-postcss',
-    "gatsby-plugin-mdx", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "posts",
-      "path": "./content/posts"
+    "gatsby-plugin-mdx",
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: "posts",
+        path: "./content/posts"
+      },
     },
-  }]
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: "images",
+        path: "./src/images"
+      },
+    }
+  ]
 };
--- src/pages/posts/{mdx.fields__slug}.js
+++ src/pages/posts/{mdx.fields__slug}.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { graphql } from 'gatsby';
-
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import { MDXProvider } from '@mdx-js/react';
-
 import PrismSyntaxHighlight from '../../components/prism-syntax-highlight';
 
 const components = {
@@ -17,13 +16,18 @@ const components = {
 
 const Post = ({
   data: {
+    mdx
   },
   children
-}) => (
+}) => {
+  let featuredImg = getImage(mdx.frontmatter.featuredImage?.path?.childImageSharp?.gatsbyImageData)
+  return (
   <MDXProvider components={components}>
+    <GatsbyImage image={featuredImg} className="w-8" />
+    <p>{mdx.frontmatter.featuredImage?.caption}</p>
     {children}
   </MDXProvider>
-);
+)}
 
 export const query = graphql`
   query ($id: String) {
@@ -31,6 +35,14 @@ export const query = graphql`
       frontmatter {
         title
         date(formatString: "MMMM Do, YYYY")
+        featuredImage {
+          path {
+            childImageSharp {
+              gatsbyImageData(width: 800)
+            }
+          }
+          caption
+        }
       }
     }
   }
