d35bfda0ce683db868f1b2651e5b95b4021c5278
Stephen Cavender <6619472+StephenCavender@users.noreply.github.com>
2022-12-22 13:31:06 -0500

    refactor: rm unused code, rm unnecessary image titles

--- src/components/root-wrapper.js
+++ src/components/root-wrapper.js
@@ -2,10 +2,5 @@ import React from 'react'
 import { Layout } from './layout'
 
 export const RootWrapper = ({ children }) => {
-  return (
-    <Layout>{children}</Layout>
-    // <main className="prose prose-sm mx-auto px-2 sm:prose sm:px-4">
-    //   {children}
-    // </main>
-  )
+  return <Layout>{children}</Layout>
 }
