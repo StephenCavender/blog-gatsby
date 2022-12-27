bdc59b7447b9cd9c1c1de9a73252e68e6d6cb367
Stephen Cavender <6619472+StephenCavender@users.noreply.github.com>
2022-12-26 23:20:41 -0500

    feat: create better header image for posts

--- src/pages/posts/{mdx.fields__slug}.js
+++ src/pages/posts/{mdx.fields__slug}.js
@@ -3,6 +3,7 @@ import { graphql } from 'gatsby'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import { MDXProvider } from '@mdx-js/react'
 import { SEO, CodeBlock } from '../../components'
+import Calendar from '../../svg/calendar.svg'
 
 const components = {
   code: CodeBlock,
@@ -13,8 +14,27 @@ const Post = ({ data: { mdx }, children }) => {
     mdx.frontmatter.featuredImage?.path?.childImageSharp?.gatsbyImageData
   )
 
+  const date = mdx.frontmatter.modifiedDate || mdx.frontmatter.date
+
   return (
     <MDXProvider components={components}>
+      <div
+        className="relative h-[220px] bg-cover bg-center bg-no-repeat p-8"
+        style={{
+          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${featuredImg.images.fallback.src})`,
+        }}
+      >
+        <h1 className="!text-2xl">{mdx.frontmatter.title}</h1>
+        <div className="flex items-center text-sm">
+          <Calendar height="0.875rem" />
+          <time>{date}</time>
+        </div>
+        {/* TOOD: separate caption from link, link entire span here */}
+        <span className="absolute bottom-0 right-0 my-0 mx-auto rounded-tl-lg bg-black py-0.5 px-1 text-xs opacity-50">
+          test
+        </span>
+      </div>
+
       <GatsbyImage alt="featured image" image={featuredImg} className="w-8" />
       <p>{mdx.frontmatter.featuredImage?.caption}</p>
       {children}
