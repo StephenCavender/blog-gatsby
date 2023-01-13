9759d3f5b6f9ecbc06116ac6cf2c77b55cfbed76
Stephen Cavender <6619472+StephenCavender@users.noreply.github.com>
2023-01-13 14:19:52 -0500

    feat: link footer name to biolink

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import Ichthys from '../svg/ichthys.svg'
+import { useSiteMetadata } from '../hooks/use-site-metadata'
 
 export const Footer = () => {
   const year = new Date().getFullYear()
+  const { biolinkUrl } = useSiteMetadata()
 
   return (
     <footer className="mt-4 flex items-center justify-center bg-surface py-6 shadow-inner">
-      © {year} Stephen Cavender <Ichthys alt="ichthys" className="w-12" />
+      © {year} &nbsp;
+      <a href={biolinkUrl} className="!no-underline">
+        Stephen Cavender
+      </a>{' '}
+      <Ichthys alt="ichthys" className="w-12" />
     </footer>
   )
 }
