9384c78d876bdfbbc81b4aa894b4982eec9b8ab8
Stephen Cavender <6619472+StephenCavender@users.noreply.github.com>
2023-03-29 14:35:18 -0400

    feat: update draft script to loop through args

--- scripts/draft.sh
+++ scripts/draft.sh
@@ -6,12 +6,17 @@
 
 DRAFTS=content/posts/drafts
 
-if [ ! -d $DRAFTS/$1 ]
-  then
-    # Copy template post into named post folder under drafts
-    cp -R $DRAFTS/template $DRAFTS/$1
-  else
-    echo "'$1' already exists in drafts"
-fi
+for draft in "$@"
+do
+  if [ ! -d $DRAFTS/$draft ]
+    then
+      # Copy template post into named post folder under drafts
+      cp -R $DRAFTS/template $DRAFTS/$draft
+    else
+      echo "'$draft' already exists in drafts"
+  fi
+done
+
+
 
 exit 0
